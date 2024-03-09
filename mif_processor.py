4625dba744e1d0625e292d916c6631ac51f76f23
hunter951 <35748952+hunter951@users.noreply.github.com>
2024-03-09 14:29:55 -0500

    Updated for baseline measurements files
    Added baseline measurements results and added to mif_processor.py the capability to print out all PLLs for each board.

--- mif_processor.py
+++ mif_processor.py
@@ -7,50 +7,80 @@ import numpy as np
 import csv
 
 # mif_processor.py
-# Produces a .csv file containing the data from the DE1-SoC RAMs stored in .mif files.  
-# Note only handles PLL_60 of first board need to expand for all
-fCount = 0
-p = Path('.')
-#boardPath = Path(".\\Shipped_Boards_Baselines\\Board_02612\\Baseline\\PLL_RAMs\\PLL_60")
-#for file in os.listdir(Path('.').joinpath('Shipped_Boards_Baselines').joinpath('Board_02612').joinpath('Baselines').joinpath('PLL_RAMs').joinpath('PLL_60')):
-#for file in os.listdir(p.joinpath('Shipped_Boards_Baselines').joinpath('Board_02612').joinpath('Baseline').joinpath('PLL_RAMs').joinpath('PLL_60')):
-#    if fnmatch.fnmatch(file, '*.mif'):
-#        fCount += 1
+# Produces a .csv file for each board containing the data from the DE1-SoC RAMs stored in .mif files.  
 
-data = []
-#for fileNum in range(1, fCount + 1):
-	#with open('RAM' + str(fileNum) + '.mif') as f:
-		#width, mem = mif.load(f, packed=True)
-		#data.append(mem)
-for fileNum in range(1, 9):
-      filePath = p.joinpath('Shipped_Boards_Baselines').joinpath('Board_02612').joinpath('Baseline').joinpath('PLL_RAMs').joinpath('PLL_60').joinpath('RAM' + str(fileNum) + '.mif')
+def write_RAMs_to_file(boardname):    
+    data = []
+    load_PLL(data, 'PLL_60', boardname)
+    load_PLL(data, 'PLL_70', boardname)
+    load_PLL(data, 'PLL_80', boardname)
+    load_PLL(data, 'PLL_90', boardname)
+    load_PLL(data, 'PLL_100', boardname)
+    load_PLL(data, 'PLL_125', boardname)
+    load_PLL(data, 'PLL_150', boardname)
+    load_PLL(data, 'PLL_155', boardname)
+    load_PLL(data, 'PLL_160', boardname)
+    load_PLL(data, 'PLL_165', boardname)
+    
+    rows = []
+    rowCursor = 0
+    for inst in range (len(data)):
+        row = []
+        if inst % 8 == 0:
+            if inst == 0:
+                rows.append(['PLL 60'])
+            elif inst == 8:
+                rows.append(['PLL 70'])
+            elif inst == 16:
+                rows.append(['PLL 80'])
+            elif inst == 24:
+                rows.append(['PLL 90'])
+            elif inst == 32:
+                rows.append(['PLL 100'])
+            elif inst == 40:
+                rows.append(['PLL 125'])
+            elif inst == 48:
+                rows.append(['PLL 150'])
+            elif inst == 56:
+                rows.append(['PLL 155'])
+            elif inst == 64:
+                rows.append(['PLL 160'])
+            elif inst == 72:
+                rows.append(['PLL 165'])                                                  
+        row.append('')
+        row.append('RAM ' + str((inst % 8) + 1))
+        row.append('Address')
+        for byteNum in range(9, 0, -1):
+            row.append('Byte ' + str(byteNum))
+        rows.append(row)
+        rowCursor += 1
+        for addr in range(len(data[inst])):
+            cols = []
+            cols.append('')
+            cols.append('')
+            #cols.append('Address ' + str(addr) + ':')
+            cols.append(str(addr))
+            for byte in range(len(data[inst][addr])):
+                cols.append(str(data[inst][addr][byte]))
+            rows.append(cols)
+            rowCursor += 1
+
+    csvfname = boardname + '_Functional_Results.csv'	
+    with open(csvfname, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(rows)
+    
+def load_PLL(dataStorage, PLLname, boardname):
+    p = Path('.')
+    for fileNum in range(1, 9):
+      filePath = p.joinpath('Shipped_Boards_Baselines').joinpath(boardname).joinpath('Baseline').joinpath('PLL_RAMs').joinpath(PLLname).joinpath('RAM' + str(fileNum) + '.mif')
       f = open(filePath, 'r')
       width, mem = mif.load(f, packed=True)
-      data.append(mem)
-      f.close()
+      dataStorage.append(mem)
+      f.close()    
 
-print(data[0])
-
-rows = []
-rowCursor = 0
-for inst in range (len(data)):
-    col = []
-    col.append('RAM ' + str(inst + 1))
-    col.append('')
-    for byteNum in range(9, 0, -1):
-        col.append('Byte ' + str(byteNum))
-    rows.append(col)
-    rowCursor += 1
-    for addr in range(len(data[inst])):
-        cols = []
-        cols.append(' ')
-        cols.append('Address ' + str(addr) + ':')
-        for byte in range(len(data[inst][addr])):
-            cols.append(str(data[inst][addr][byte]))
-        rows.append(cols)
-        rowCursor += 1
-		
-with open('test.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerows(rows)
-    
+write_RAMs_to_file('Board_02612')
+write_RAMs_to_file('Board_02689')
+write_RAMs_to_file('Board_02748')
+write_RAMs_to_file('Board_03104')
+write_RAMs_to_file('Board_10970')
